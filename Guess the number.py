57ea97ced6cda1881b8620cbaa13fedba67e9d20
nonook <104591876+Nonook-3352@users.noreply.github.com>
2024-03-14 13:33:52 +0100

    optimised unoptimised code.

--- Guess the number.py
+++ Guess the number.py
@@ -1,109 +1,34 @@
 import random 
 
+inp = input("Choisi un nombre de tentative : ")
+win = False
 #début
 print("BIENVENUE DANS : Guess the number !")
 print(" ")
-print("Tu as 10 tentavive de trouvez le nombres mystère qui se trouvent entre 1 et 100.")
+print(f"Tu as {inp} tentavive de trouvez le nombres mystère qui se trouvent entre 1 et 100.")
 print(" ")
 
 #nombre choisi
 number = random.randint(1, 100)
 
 
-#debug
-
-#input
-choix = input("donnez un nombre : ")
-#var pour boucle infini
 
 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)
-        
-        
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)       
-        
+for i in range(0, int(inp)):
+        #input
+        choix = input("donnez un nombre : ")
+        #var pour boucle infini
 
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)       
-        
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)       
 
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)       
-        
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)
-
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)
-
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)
-
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)
-
-choix = input("donnez un nombre : ") 
-if int(choix) < number :
-        print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
-if int(choix) > number :
-        print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
-if int(choix) == number :
-        print("bravo tu a trouvez le nombre mystère") 
-        print(number)               
+        if int(choix) < number :
+                print("Le nombre que tu a proposez est plus petit que le nombre mystère.")
+        if int(choix) > number :
+                print("Le nombre que tu a proposez est plus grand que le nombre mystère.")
+        if int(choix) == number :
+                print("bravo tu a trouvez le nombre mystère") 
+                print(number)
+                win = True
+                
+                
+if win != True:
+    print("Tu as perdu !")
